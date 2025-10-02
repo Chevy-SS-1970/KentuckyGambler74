4f63fcba2adaf44be55f77a0184d3c95419fa738
Chevy-SS-1970 <vladv20102@gmail.com>
2025-10-02 15:24:21 +0300

    Добавлены функции create() и delete().

--- Projects/Project 1/manager_of_projects.py
+++ Projects/Project 1/manager_of_projects.py
@@ -1,7 +1,7 @@
 def interface():
-    print('Greetings, my Lord! We praise thee.')
+    print('Greetings, my Lord! We praise you.')
     while True:
-        print('''What do you want me to do?
+        print('''What do you want us to do?
                  1. Create a new note
                  2. Delete your note
                  3. Search for a note
@@ -26,12 +26,34 @@ def interface():
 
 
 def create():
-    pass
+    note = input('Type a new note: ')
+    with open('notes.txt', 'a', encoding = 'UTF-8') as file:
+        file.write(note + '\n')
+    print('Your note was successfully created! \n')
 
 
 
 def delete():
-    pass
+    with open('notes.txt', 'r', encoding = 'UTF-8') as file:
+        notes = file.readlines()
+    if not notes:
+        print('You have not created any notes yet. \n')
+        return None
+    
+    print('List of your notes: ')
+    for i, n in enumerate(notes, 1):
+        print(f'{i}. {n}')
+    note_index = int(input('Which note do you want to delete? — '))
+    if note_index > 0 and len(notes) >= note_index:
+        notes_except_deleted = []
+        for i in range(len(notes)):
+            if note_index != i+1:
+                notes_except_deleted.append(notes[i])
+        with open('notes.txt', 'w', encoding = 'UTF-8') as file:
+            file.writelines(notes_except_deleted)
+        print('Your note was successfully deleted! \n')
+        return None
+    print(f'There is no note with number "{note_index}". \n')
 
 
 
